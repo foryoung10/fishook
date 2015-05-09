ea3fb7bdeeb645ef57b14f767608511e36ee54df
foryoung10 <foryoung89@gmail.com>
2015-05-09 13:22:29 +0800

    Initial Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,39 @@
+angular.module('starter.controllers', [])
+
+.controller('MainCtrl', function($scope) {})
+
+.controller('LocationCtrl', function($scope, $cordovaGeolocation) {
+
+      $scope.getLocation = function() {
+      var posOptions = {timeout: 10000, enableHighAccuracy: false};
+      $cordovaGeolocation
+          .getCurrentPosition(posOptions)
+          .then(function (position) {
+            $scope.lat = position.coords.latitude
+            $scope.long = position.coords.longitude
+          }, function (err) {
+            // error
+          });
+
+
+      var watchOptions = {
+        frequency: 1000,
+        timeout: 3000,
+        enableHighAccuracy: false // may cause errors if true
+      };
+    }
+})
+
+.controller('ChatsCtrl', function($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
